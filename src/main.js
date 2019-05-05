0a8ecfa3a9aea4591d7bbc0a9f645f44911245e2
qqqqqqian <1260089994@qq.com>
2019-05-05 17:13:41 +0800

    init my project

---
+++ src/main.js
@@ -0,0 +1,20 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import fastClick from 'fastclick'
+import App from './App'
+import router from './router'
+import 'styles/reset.css'
+import 'styles/border.css'
+import 'styles/iconfont.css'
+
+Vue.config.productionTip = false
+
+fastClick.attach(document.body)
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>' // 这样写会直接替换掉el中的节点
+})
---
+++ src/pages/home/components/Header.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="header">
+    <div class="headerLeft"><i class="iconfont">&#xe624;</i></div>
+    <div class="headerInput"><i class="iconfont">&#xe632;</i>输入城市/景点/游玩主题</div>
+    <div class="headerRight">地区<i class="iconfont">&#xe62d;</i></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeHeader'
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import "~styles/common.styl"
+  .header
+    display:flex
+    justify-content: center
+    align-items: center
+    height:.44rem
+    width: 100%
+    background-color: $headerColor
+    font-size:.14rem
+    color:#fff
+    .headerLeft
+      width:.4rem
+      float:left
+      line-height:.44rem
+      text-align:center
+    .headerRight
+      width:.78rem
+      float:right
+      line-height:.44rem
+      text-align:center
+    .headerInput
+      padding-left:.05rem
+      flex:1
+      line-height:.3rem
+      height:.3rem
+      background-color: #fff
+      border-radius:.05rem
+      color:#e4e7ea
+</style>
