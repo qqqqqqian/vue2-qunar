7f47cbeb1ec02ee26dce704eda31cc062f5a148f
qqqqqqian <1260089994@qq.com>
2019-05-05 20:52:01 +0800

    change

--- src/main.js
+++ src/main.js
@@ -4,17 +4,19 @@ import Vue from 'vue'
 import fastClick from 'fastclick'
 import App from './App'
 import router from './router'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
 import 'styles/reset.css'
 import 'styles/border.css'
 import 'styles/iconfont.css'
-
+import '../node_modules/swiper/dist/css/swiper.css'
 Vue.config.productionTip = false
-
+Vue.use(VueAwesomeSwiper)
 fastClick.attach(document.body)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
-  components: { App },
+  components: {
+    App},
   template: '<App/>' // 这样写会直接替换掉el中的节点
 })
---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="wrapper">
+      <swiper :options="swiperOption">
+        <!-- slides -->
+        <swiper-slide v-for="item in items" :key="item.id"><img class="swiper-img"  :src="item.img"  alt=""></swiper-slide>
+        <!-- Optional controls -->
+        <div class="swiper-pagination"  slot="pagination"></div>
+      </swiper>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        autoplay: {
+          delay: 3000,
+          stopOnLastSlide: false,
+          disableOnInteraction: false},
+        loop: true,
+        pagination: {
+          el: '.swiper-pagination'
+        }
+      },
+      items: [
+        {id: '0001',
+          img: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20193/d7bbc21db442366a882e04ddc984669a.jpg_750x200_85e640d9.jpg'
+        },
+        {id: '0002',
+          img: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20193/87a224d0349d94a11e97f31aa1aba4f5.jpg_750x200_1f78af87.jpg'
+        },
+        {id: '0003',
+          img: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20194/e0c5d91cb9b435518bb223b81b70874e.jpg_750x200_f2d5c66a.jpg'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .wrapper >>> .swiper-pagination-bullet-active
+    background: #fff
+  .wrapper
+    width: 100%
+    height:0
+    padding-bottom:26%
+    .swiper-img
+      width:100%
+    .swiper-pagination
+      margin-bottom:-0.05rem
+</style>
