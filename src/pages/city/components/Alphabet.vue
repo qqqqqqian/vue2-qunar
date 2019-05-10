aa1de9027a6461c8a6b1fe52b5dd3fc02f5bd7b7
qqqqqqian <1260089994@qq.com>
2019-05-10 11:29:13 +0800

    search logic

--- src/pages/city/components/Alphabet.vue
+++ src/pages/city/components/Alphabet.vue
@@ -1,14 +1,56 @@
 <template>
   <ul class="alphabet-wrapper">
-    <li v-for="item of alphabetAry" :key="item">{{item}}</li>
+    <li
+      v-for="item of alphabetAry"
+      :key="item"
+      :ref="item"
+      @click="letterClick"
+      @touchstart="touchstart"
+      @touchmove="touchmove"
+      @touchend="touchend"
+    >{{item}}
+    </li>
   </ul>
 </template>
 
 <script>
 export default {
   name: 'Alphabet',
+  data () {
+    return {
+      touchStatus: false
+    }
+  },
   props: {
     alphabetAry: Array
+  },
+  updated () {
+    this.aTop = this.$refs['A'][0].getBoundingClientRect().top
+    this.aHeight = this.$refs['A'][0].getBoundingClientRect().height
+  },
+  methods: {
+    letterClick (e) {
+      this.$emit('change', e.target.innerText)
+    },
+    touchstart (e) {
+      this.touchStatus = true
+    },
+    touchmove (e) {
+      if (this.moveTimer) {
+        clearTimeout(this.moveTimer)
+      }
+      this.moveTimer = setTimeout(() => {
+        if (this.touchStatus) {
+          const index = Math.floor((e.changedTouches[0].clientY - this.aTop) / this.aHeight)
+          if (index >= 0 && index < this.alphabetAry.length) {
+            this.$emit('change', this.alphabetAry[index])
+          }
+        }
+      }, 18)
+    },
+    touchend (e) {
+      this.touchStatus = false
+    }
   }
 }
 </script>
@@ -26,6 +68,7 @@ export default {
     flex-direction: column
     justify-content: center
     align-items: center
+
     li
       line-height: .2rem
       width: 100%
